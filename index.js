04bf2451ea827cd74d54c611e5eb32c5da1aec26
Sean Wilson <hello@imsean.me>
2016-10-21 18:09:09 -0500

    Fixed crashing issue when request failed

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-var request = require('https').request;
+var https = require('https');
 var Promise = require('any-promise');
 var routine = require('promise-routine');
 
 module.exports = function slack(input) {
   return routine(function getStatus(team) {
-    return new Promise(function(resolve) {
-      request({
+    return new Promise(function(resolve, reject) {
+      var req = https.request({
         hostname: team + '.slack.com',
         path: '/'
       }, function(resp) {
         resolve(['slack', team, resp.statusCode === 404 ? 'free' : 'taken']);
-      }).end();
+      });
+
+      req.end();
+      req.on('error', e => reject(e));
     });
   }, input);
 };
